7c91ec27acd2ce97d3638a79d3975a4bb6588d37
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-10-14 10:43:56 +0200

    [IMP] Problem 2 name refactor

--- src/main/scala/Problem002.scala
+++ src/main/scala/Problem002.scala
@@ -6,8 +6,8 @@
 object Problem002 extends App {
   def fibSum (a :Int, b :Int) :Int =
     (if (b % 2 == 0) b else 0) + (if (a+b > 4000000) 0 else fibSum(b, a + b))
-  def answer : Int = fibSum(1, 2)
-  println(answer)
+  def result : Int = fibSum(1, 2)
+  println(result)
 
 }
 
