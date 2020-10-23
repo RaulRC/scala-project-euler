f87f082d2787bf26ae3403bb734517722afbd992
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-10-23 09:25:28 +0200

    [ADD] Problem 9 solved

---
+++ src/main/scala/Problem009.scala
@@ -0,0 +1,17 @@
+/* 
+ * @project project-euler
+ * @author raul.reguillo on 2020-10-23
+ */
+
+object Problem009 extends App{
+  val limit = 1000
+
+  println(
+    (for {
+    a <- 1 to limit
+    b <- 1 to limit
+    c <- 1 to limit
+    if a*a + b*b == c*c && a + b + c == limit
+    } yield a * b * c).head)
+
+}
