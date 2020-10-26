804a2ca86737158f820b09313d2ad576bc2802e4
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-10-26 16:55:19 +0100

    [IMP] update problem 10 for efficiency

--- src/main/scala/Problem010.scala
+++ src/main/scala/Problem010.scala
@@ -6,13 +6,16 @@
  */
 
 object Problem010 extends App{
+
+  val LIMIT = 2000000
+
   def odds(n:Long): Stream[Long] = Stream.cons(n,odds(n + 2))
 
   def isPrime(n:Long): Boolean =
     (primes takeWhile { _ <= math.floor(math.sqrt(n)) }) forall { n % _ != 0 }
   lazy val primes: Stream[Long] = Stream.cons(2,odds(3) filter isPrime)
   println(
-    (primes takeWhile { _ < 2000000 }).toList.sum
+    (primes takeWhile { _ < LIMIT }).toList.sum
   )
 }
 
