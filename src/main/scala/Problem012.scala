65d42224fe51337d0df9f5ce9522b820d4595d13
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-10-28 15:40:26 +0100

    [ADD] Problem 12 solved

---
+++ src/main/scala/Problem012.scala
@@ -0,0 +1,19 @@
+/*
+ * @project project-euler
+ * @author raul.reguillo on 2020-10-28
+ */
+
+object Problem012 extends App{
+  val N_DIVISORS = 500
+
+  val triangular = Stream.from(1).scan(0)(_ + _)
+  def divisors(n: Int): Int = {
+    val middleFactor = math.sqrt(n).intValue
+    val smallFactors = (1 to middleFactor).count(n % _ == 0)
+    2 * smallFactors - (if (middleFactor * middleFactor == n) 1 else 0)
+  }
+  println(
+    triangular.find(divisors(_) > N_DIVISORS).head
+  )
+
+}
