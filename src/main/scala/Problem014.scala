423e42f6becee3be3342231412ac0df02b2b7433
Raúl Reguillo Carmona <raul.reguillo@gmail.com>
2020-10-30 10:44:30 +0100

    [ADD] Problem 14 solved

--- src/main/scala/Problem014.scala
+++ src/main/scala/Problem014.scala
@@ -3,6 +3,20 @@
  * @author raul.reguillo on 2020-10-30
  */
 
-object Problem014 {
+object Problem014 extends App{
+  val LIMIT = 1000000
 
+  //def seq(n: Int): Stream[Int] = if (n == 1) Stream(n) else Stream.cons(n, if (n % 2 == 0) seq(n/2) else seq(3 * n+1))
+
+  def seq(n: Long, c: Int = 0): Int =
+    if (n == 1) c + 1
+    else
+      seq(
+        if (n % 2 == 0) n / 2
+        else 3 * n + 1,
+        c + 1)
+
+  val r = (1 until LIMIT).view.map(n => (n, seq(n))).reduceLeft((a, b) => if (a._2 > b._2) a else b)._1
+
+  println(r)
 }
